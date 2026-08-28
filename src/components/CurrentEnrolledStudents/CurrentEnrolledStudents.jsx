6be0a64f28c771432b3e96bd1e3f6ba2a4bb1eb2
Suneha Mariam <rehanagill260@gmail.com>
2026-08-28 22:01:21 +0500

    Fix import path for CurrentEnrolledStudents component

--- src/components/CurrentEnrolledStudents/CurrentEnrolledStudents.jsx
+++ src/components/CurrentEnrolledStudents/CurrentEnrolledStudents.jsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from "react"
-import "./CurrentEnrolledStudents.css"
+import "../CurrentEnrolledStudents/CurrentEnrolledStudents"
 
 const CurrentEnrolledStudents = () => {
 
